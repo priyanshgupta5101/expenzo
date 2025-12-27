5ee15cef0d387d79cd6ae80aa434ad7727139ba9
Priyansh Gupta <priyanshgupta5101@gmail.com>
2025-12-27 20:24:45 +0530

    Delete all files from repository

--- .next/static/chunks/app_(main)_groups_[id]_page_jsx_abc11d28._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_0add25aa._.js",
-  "static/chunks/node_modules_4716d0b3._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_(main)_settlements_[type]_[id]_page_jsx_abc11d28._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_3482df42._.js",
-  "static/chunks/node_modules_0673398e._.js"
-],
-    source: "dynamic"
-});
